f8e41f2478d0a5c91fe9bd7808cc1cab3bccd603
nicosharpe <nicolesharpe11@gmail.com>
2024-01-16 13:10:39 +0000

    up to highscore / local storage part

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,83 +1,209 @@
-// start button - on click, timer starts and 1st question appears
-// Attach event listener to increment button element
 var startBtn = document.querySelector("#start");
+var timeEl = document.querySelector("#time");
 var choices = document.querySelector("#choices");
-var timer = document.querySelector("#time")
-var seconds = 60
+var end = document.querySelector("#end-screen");
+var score = end.getElementsByTagName("#final-score");
 
-startBtn.addEventListener("click", function() {
-  setInterval(function () {
-    seconds -= 1;
-    timer.textContent = seconds;
-console.log(seconds);
-  }, 1000);
-  
-  function timer(){
-    // Update the count down every 1 second
-//
-    // var sec = 60;
+console.log(score);
 
-  }
-  // count++;
-  // setCounterText();
-});
+var currentQuestionIndex = 0
+var secondsLeft = 60;
+var userScore = 0
 
-// timer
-
-console.log(timer);
-
-// questions - contain buttons for each answer
-// when answer is clicked, next question appears
-//if incorrect answer, subtract time from clock
 var questions = [
   {
-  question1:'How many javascript data-types are there?',
-  answers1: ["5","100","50","85"],
+    question: 'What is JavaScript primarily used for in web development?',
+    answers: ["Styling", "Animation", "Client-side scripting", "Database management"],
+    // client side scripting
+    correctAnswer: 2
+  },
 
-  question2:'How many javascript data-types are there?',
-  answers2: ["5","100","50","85"],
+  {
+    question: 'What does DOM satand for?',
+    answers: ["Document Object Model", "Data Object Model", "Document Orientation Model", "Document Order Model"],
+    //document object model
+    correctAnswer: 0
+  },
 
-  question3:'How many javascript data-types are there?',
-  answers3: ["5","100","50","85"],
+  {
+    question: 'Which of the following is used to loop through elements in an array in JavaScript?',
+    answers: ["forEach", "iterate", "loop", "traverse"],
+    //forEach
+    correctAnswer: 0
+  },
 
-  question4:'How many javascript data-types are there?',
-  answers4: ["5","100","50","85"],
+  {
+    question: 'What is the purpose of the localStorage object in JavaScript?',
+    answers: ["Store data that will be cleared when the browser is closed", "Store data that persists even when the browser is closed", "Store only numerical data", "Store server-side data"],
+    //store data that persists even when the browser is closed
+    correctAnswer: 1
+  },
 
-  question5:'How many javascript data-types are there?',
-  answers5: ["5","100","50","85"],
-
-}
 ]
 
+// start button - on click, timer starts and 1st question appears
+startBtn.addEventListener("click", function () {
+  ;
 
+  // Set interval in variable
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if ((secondsLeft === 0) || (currentQuestionIndex >= questions.length)) {
+      // Stop execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append message
+      sendMessage();
+    }
+
+  }, 1000);
+
+  questionTime(currentQuestionIndex);
+});
+
+
+// removes starting page after click (onclick in html)
+function startQuiz() {
+  var quizStart = document.querySelector("#start-screen");
+  if (quizStart.style.display === "none") {
+    quizStart.style.display = "block";
+  } else {
+    quizStart.style.display = "none";
+  }
+}
+
+var QSection = document.querySelector("#questions")
+
+function questionTime(index) {
+  QSection.classList.remove("hide");
+  var QTitle = QSection.getElementsByTagName("h2")[0];
+  QTitle.textContent = questions[index].question;
+  var answers = questions[index].answers
+  choices.innerHTML = "";
+
+  for (let i = 0; i < answers.length; i++) {
+
+    var answerBtn = document.createElement("BUTTON");
+    choices.appendChild(answerBtn);
+    answerBtn.textContent = answers[i];
+    answerBtn.dataset.index = i;
+    answerBtn.onclick = checkAnswer;
+  }
+
+
+  console.log(questions[0].question);
+
+
+}
+
+function checkAnswer(event) {
+  var answerIndex = event.target.dataset.index;
+  const question = questions[currentQuestionIndex];
+  if (answerIndex === question.correctAnswer) {
+    userScore++;
+  } else {
+    secondsLeft -= 10;
+  }
+
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    questionTime(currentQuestionIndex);
+  } else {
+    showResults();
+
+  }
+
+
+}
+
+function showResults() {
+  QSection.classList.add("hide")
+  end.classList.remove("hide")
+}
+// Function to create and append message
+function sendMessage() {
+
+  var timesUpTitle = end.getElementsByTagName("h2")[0];
+  timesUpTitle.textContent = "Times Up!";
+  score.textContent = secondsLeft;
+}
+
+var submit = document.querySelector("#submit");
+
+submit.addEventListener('click', function (event) {
+  function saveUserScore() {
+    let initials = document.querySelector("#initials"); // the text that user submits
+
+
+console.log(score);
+
+    localStorage.setItem("Initials", initials);
+    localStorage.setItem("score", score);
+
+    let userScore = initials + score
+    console.log(userScore);
+
+
+
+  }
+});
+
+
+
+
+
+// data we capture from the user input and score
+// let tempUser = { name: "test", score: 12 }
+// type of tempUser --> JS OBJECT  | typeof tempUser
+
+// highScores = [{}, {}]
+
+// JSON --> "{ "name": "test", "score": 12 }"
+// JSON.stringify(jsObj)  --> JSON OBJ "{ }"
+// JSON.parse(jsonData)  --> JavaScript OBJ
+var highscores = [{ name: "test", score: 12 }, { name: "test1", score: 6 }];
+JSON.stringify(highscores);
+
+// -- localStorage -- 
+// setItem(key, data);  --> sets key and data
+// getItem(key) --> retrieves data stored
+function populateStorage() {
+  setItem(highscores,)
+}
+
+
+
+
+
+// -- Order of Operations
+// How do we deal with or interact with our data(?)
+// --> in localStorage (KEY: VALUE) --> highscores: []
+// Q) How do we initalize localStorage DATA(?)
+// ---> setItem('highScores', JSON.stringify([]);
+// Q) How would we update the highscores(?) --> What ACTION triggers this logic(?)
+
+// --> submit event to capture user initials 
+// data we capture from the user input and score
+// let tempUser = { name: "test", score: 12 }
+// type of tempUser --> JS OBJECT  | typeof tempUser
+
+// Q) what steps do we need to keep in mind to update localStorage(?)
+// --> we need to retrieve stored data | let jsonData = localStorage.getItem(key);
+// data = "[]"  --> STRING OBJECT (JSON)
+// --> CONVERT that data into something more useable (jsOBJ)
+// let jsDATA = JSON.parse(jsonObj) --> []  --> typeof 
+// --> Manipulate the data (Create Read Update Delete)
+// jsData.push(tempUser)  | filter to remove 
+// --> CONVERT it BACK to a STRING(JSONobj)
+// let jsonData = JSON.stringify(jsData);
+// --> SAVE the updated dataset to localStorage
+// localStorage.setItem(key, jsanData)  --> WRITEING DATA TO THE BROWSER
+
+
+console.log(questions);
 
 // quiz ends when all questions are answered OR timer reaches zero
 // when game ends it should display score and give user ability to save initials and score
 // local storage
 
-
-
-// var count = 0;
-// //  Select increment and decrement button elements
-// var incrementEl = document.querySelector("#increment");
-// var decrementEl = document.querySelector("#decrement");
-// var countEl = document.querySelector("#count");
-
-// // Updates count on page
-// function setCounterText() {
-//   countEl.textContent = count;
-// }
-// // Attach event listener to increment button element
-// incrementEl.addEventListener("click", function() {
-//   count++;
-//   setCounterText();
-// });
-
-// // Attach event listener to decrement button element
-// decrementEl.addEventListener("click", function() {
-//   // Action will fire if count is greater than  0
-//   if (count > 0) {
-//     count--;
-//     setCounterText();
-//   }
-// });
--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -4,3 +4,28 @@
 // quiz ends when all questions are answered OR timer reaches zero
 // when game ends it should display score and give user ability to save initials and score
 // local storage
+
+
+// Q) HOW do we retrieve stored Data(?)
+// let jsonData = localStorage.getItem(key)
+var submit = document.querySelector("#submit");
+var end = document.querySelector("#end-screen");
+var score = end.getElementsByTagName("#final-score");
+
+submit.addEventListener('click', function (event) {
+  function saveUserScore() {
+    let initials = document.querySelector("#initials"); // the text that user submits
+
+
+
+
+    localStorage.setItem("Initials", initials);
+    localStorage.setItem("Score", score);
+
+    let userScore = initials + score
+    console.log(userScore);
+
+
+
+  }
+});
